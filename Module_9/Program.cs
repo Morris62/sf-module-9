ab24c5fd7ff94d0d59f43822e3cbc741ff4e1ffe
a.chaturov <chaturovai@gmail.com>
2025-02-23 22:55:09 +0300

    unit 9.1.4

---
+++ Module_9/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Module_9;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+      var exception = new Exception("Тест исключения");
+      exception.Data.Add("Время возникновения исключения", DateTime.Now);
+      exception.HelpLink = "www.ya.ru";
+    }
+}
