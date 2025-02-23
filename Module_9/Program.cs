1a2c1a98653ccdf9689282170346be1f6dba89bc
a.chaturov <chaturovai@gmail.com>
2025-02-23 23:37:30 +0300

    unit 9.2.3

--- Module_9/Program.cs
+++ Module_9/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Module_9;
+﻿using System.ComponentModel;
+
+namespace Module_9;
 
 class Program
 {
@@ -11,8 +13,16 @@ class Program
             //2// string str = null;
             //2// var x = str.Length;
             //3// throw new ArgumentOutOfRangeException();
-            //4//
-            throw new RankException();
+            //4// throw new RankException();
+
+            try
+            {
+                Method2();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.StackTrace);
+            }
         }
         catch (Exception ex) when (ex.Message == "Ошибка связи с БД")
         {
@@ -37,6 +47,30 @@ class Program
         finally
         {
             Console.WriteLine("Блок finally");
+        }
+
+        static void Method1()
+        {
+            try
+            {
+                throw new WarningException();
+            }
+            catch (Exception e)
+            {
+                throw;
+            }
+        }
+
+        static void Method2()
+        {
+            try
+            {
+                Method1();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
         }
 
         Console.ReadKey();
