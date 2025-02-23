77823d30c47d2ef44c21a254c46fbec80a97f151
a.chaturov <chaturovai@gmail.com>
2025-02-23 23:55:26 +0300

    unit 9.3.2

--- Module_9/Program.cs
+++ Module_9/Program.cs
@@ -1,78 +1,13 @@
-﻿using System.ComponentModel;
-
-namespace Module_9;
+﻿namespace Module_9;
 
 class Program
 {
+    delegate int DiffDelegate(int a, int b);
+    static int CalculateDiff(int a, int b) => a - b;
+
     static void Main(string[] args)
     {
-        try
-        {
-            Console.WriteLine("Блок try");
-            //1// throw new Exception("Ошибка связи с БД");
-            //2// string str = null;
-            //2// var x = str.Length;
-            //3// throw new ArgumentOutOfRangeException();
-            //4// throw new RankException();
-
-            try
-            {
-                Method2();
-            }
-            catch(Exception ex)
-            {
-                Console.WriteLine(ex.StackTrace);
-            }
-        }
-        catch (Exception ex) when (ex.Message == "Ошибка связи с БД")
-        {
-            Console.WriteLine("Блок catch - Пользовательский");
-            Console.WriteLine(ex.Message);
-        }
-        catch (Exception ex) when (ex is NullReferenceException)
-        {
-            Console.WriteLine("Блок catch - NullReferenceException");
-            Console.WriteLine(ex.Message);
-        }
-        catch (Exception ex) when (ex is ArgumentOutOfRangeException)
-        {
-            Console.WriteLine("Блок catch - ArgumentOutOfRangeException");
-            Console.WriteLine(ex.Message);
-        }
-        catch (Exception ex) when (ex is RankException)
-        {
-            Console.WriteLine("Блок catch - RankException");
-            Console.WriteLine(ex.Message);
-        }
-        finally
-        {
-            Console.WriteLine("Блок finally");
-        }
-
-        static void Method1()
-        {
-            try
-            {
-                throw new WarningException();
-            }
-            catch (Exception e)
-            {
-                throw;
-            }
-        }
-
-        static void Method2()
-        {
-            try
-            {
-                Method1();
-            }
-            catch (Exception)
-            {
-                throw;
-            }
-        }
-
-        Console.ReadKey();
+        DiffDelegate diffDelegate = CalculateDiff;
+        Console.WriteLine(diffDelegate.Invoke(10, 5));
     }
 }
