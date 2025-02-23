40efa09a2636fe736ebd190ceb714579a6369981
a.chaturov <chaturovai@gmail.com>
2025-02-23 23:37:30 +0300

    unit 9.2.3

--- Module_9/Program.cs
+++ Module_9/Program.cs
@@ -4,8 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-      var exception = new Exception("Тест исключения");
-      exception.Data.Add("Время возникновения исключения", DateTime.Now);
-      exception.HelpLink = "www.ya.ru";
+        try
+        {
+            Console.WriteLine("Блок try");
+            //1// throw new Exception("Ошибка связи с БД");
+            //2// string str = null;
+            //2// var x = str.Length;
+            //3// throw new ArgumentOutOfRangeException();
+            //4//
+            throw new RankException();
+        }
+        catch (Exception ex) when (ex.Message == "Ошибка связи с БД")
+        {
+            Console.WriteLine("Блок catch - Пользовательский");
+            Console.WriteLine(ex.Message);
+        }
+        catch (Exception ex) when (ex is NullReferenceException)
+        {
+            Console.WriteLine("Блок catch - NullReferenceException");
+            Console.WriteLine(ex.Message);
+        }
+        catch (Exception ex) when (ex is ArgumentOutOfRangeException)
+        {
+            Console.WriteLine("Блок catch - ArgumentOutOfRangeException");
+            Console.WriteLine(ex.Message);
+        }
+        catch (Exception ex) when (ex is RankException)
+        {
+            Console.WriteLine("Блок catch - RankException");
+            Console.WriteLine(ex.Message);
+        }
+        finally
+        {
+            Console.WriteLine("Блок finally");
+        }
+
+        Console.ReadKey();
     }
 }
