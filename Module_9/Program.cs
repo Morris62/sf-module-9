e2b804f16a3dfdf61720890082de15fd6cbcdf95
a.chaturov <chaturovai@gmail.com>
2025-02-24 23:50:41 +0300

    unit 9.3.13 - screencast

--- Module_9/Program.cs
+++ Module_9/Program.cs
@@ -1,19 +1,34 @@
 ﻿namespace Module_9;
 
-class Program
+public class Program
 {
-    // Анонимные методы
-    // профит - экономия времени
-    // нюансы - доступ к переменным во внешней среде
-
-    delegate int RandomNumberDelegate();
-
     static void Main(string[] args)
     {
-        RandomNumberDelegate randomNumberDelegate = delegate()
+        var employee1 = new Employee { Id = 1, Name = "Иван", Experience = 2, Salary = 10_000 };
+        var employee2 = new Employee { Id = 2, Name = "Петр", Experience = 5, Salary = 120_000 };
+        var employee3 = new Employee { Id = 2, Name = "Степан", Experience = 15, Salary = 210_000 };
+        var employees = new List<Employee>
         {
-            return new Random().Next(0, 100);
+            employee1, employee2, employee3
         };
-        Console.WriteLine(randomNumberDelegate.Invoke().ToString("000"));
+
+        Employee.PromoteEmployee(employees, employee => employee.Experience >= 5);
+    }
+}
+
+public class Employee
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int Experience { get; set; }
+    public int Salary { get; set; }
+
+    public static void PromoteEmployee(List<Employee> employees, Predicate<Employee> isEmployeeEligible)
+    {
+        foreach (var employee in employees)
+        {
+            if (isEmployeeEligible(employee))
+                Console.WriteLine($"Сотрудник {employee.Name} повышен");
+        }
     }
 }
