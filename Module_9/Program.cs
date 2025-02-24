d206def9d88a8fcf558081bc626bcde40aa756f7
a.chaturov <chaturovai@gmail.com>
2025-02-24 23:09:58 +0300

    unit 9.3.11

--- Module_9/Program.cs
+++ Module_9/Program.cs
@@ -2,35 +2,26 @@
 
 class Program
 {
-    delegate void ShowMessageDelegate();
-    delegate int SumDelegate(int a, int b, int c);
-    delegate bool CheckLengthDelegate(string _row);
-    
+    // Анонимные методы
+    // профит - экономия времени
+    // нюансы - доступ к переменным во внешней среде
+
+    public delegate string GreetingDelegate(string name);
+
+    public static string Greeting(string name)
+    {
+        return $"Привет @{name}! Добро пожаловать на SkillFactory!";
+    }
+
     static void Main(string[] args)
     {
-        Action showMessageDelegate = ShowMessage;
-        showMessageDelegate();
-        
-        Func<int,int,int,int> sumDelegate = Sum;
-        var result = sumDelegate(10, 20, 30);
-        Console.WriteLine(result);
-        
-        Predicate<string> checkLengthDelegate = CheckLength;
-        var status = checkLengthDelegate.Invoke("skill_factory");
-        Console.WriteLine(status);
-    }
-    static void ShowMessage()
-    {
-        Console.WriteLine("Hello World!");
-    }
-    static int Sum(int a, int b, int c)
-    {
-        return a + b + c;
-    }
-    static bool CheckLength(string _row)
-    {
-        if (_row.Length > 3)
-            return true;
-        return false;
+        string exMessage = "Внешний мир!";
+        //GreetingDelegate greetingDelegate = Greeting;
+        GreetingDelegate greetingDelegate = delegate(string name)
+        {
+            return $"{exMessage}Привет @{name}! Добро пожаловать на SkillFactory!";
+        };
+        string greetingMessage = greetingDelegate.Invoke("Будущий гуру");
+        Console.WriteLine(greetingMessage);
     }
 }
