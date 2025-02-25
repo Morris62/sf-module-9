2b399a28c6b7ce57d20b75333c4b4e1d4c007b03
a.chaturov <chaturovai@gmail.com>
2025-02-26 00:31:45 +0300

    unit 9.5.1

--- Module_9/Program.cs
+++ Module_9/Program.cs
@@ -1,40 +1,61 @@
 ﻿namespace Module_9;
 
-public delegate void Notify();
-
-/// <summary>
-/// Издатель
-/// </summary>
-public class ProcessBuisnessLogic
-{
-    public event Notify ProcessCompleted;
-
-    public void StartProcee()
-    {
-        Console.WriteLine("Процесс начат");
-        OnProcessingComleted();
-    }
-
-    public virtual void OnProcessingComleted()
-    {
-        ProcessCompleted?.Invoke();
-    }
-}
-
-/// <summary>
-/// Подписчик
-/// </summary>
 class Program
 {
     static void Main(string[] args)
     {
-        var bl = new ProcessBuisnessLogic();
-        bl.ProcessCompleted += bl_ProcessComplete;
-        bl.StartProcee();
+        var numberReader = new NumberReader();
+        numberReader.NumberEnteredEvent += ShowNumber;
+        while (true)
+        {
+            try
+            {
+                numberReader.Read();
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Введено некорректное значение!");
+            }
+        }
+
+        static void ShowNumber(int number)
+        {
+            switch (number)
+            {
+                case 1:
+                    Console.WriteLine("Введено значение 1");
+                    break;
+                case 2:
+                    Console.WriteLine("Введено значение 2");
+                    break;
+            }
+        }
     }
 
-    public static void bl_ProcessComplete()
+    class NumberReader
     {
-        Console.WriteLine("Процесс завершен");
+        public delegate void NumberEnteredDelegate(int number);
+
+        public event NumberEnteredDelegate NumberEnteredEvent;
+
+        public void Read()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Введите значение 1 или 2");
+
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            if (number != 1 && number != 2)
+            {
+                throw new FormatException();
+            }
+
+            NumberEntered(number);
+        }
+
+        protected virtual void NumberEntered(int number)
+        {
+            NumberEnteredEvent?.Invoke(number);
+        }
     }
 }
