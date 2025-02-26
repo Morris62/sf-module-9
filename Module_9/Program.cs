0c0c6c4c7f511036d9048ce9b5d96ab91dab0341
a.chaturov <chaturovai@gmail.com>
2025-02-26 23:08:42 +0300

    unit 9.6.1 HW-03

--- Module_9/Program.cs
+++ Module_9/Program.cs
@@ -2,34 +2,129 @@
 
 class Program
 {
+    private sealed class UserException : Exception
+    {
+        public UserException(string message = "Пользовательское исключение!") : base(message)
+        {
+            Data.Add("Время возникновения исключения", DateTime.Now);
+            HelpLink = "www.google.ru";
+        }
+    }
+
     static void Main(string[] args)
     {
+        #region Задание 1
+
+        Console.WriteLine(new string('-', 50));
+        Console.WriteLine(" Задание №1");
+        Console.WriteLine(new string('-', 50));
+
+        var exceptions = new Exception[]
+        {
+            new FormatException("Ошибка! Неверный формат!"),
+            new DivideByZeroException("Ошибка! Деление на 0!"),
+            new IndexOutOfRangeException("Ошибка! Индекс вне диапазона!"),
+            new NullReferenceException("Ошибка! Обращение к null"),
+            new UserException()
+        };
+
+        Console.WriteLine("Начало генерации исключений");
+        foreach (var exception in exceptions)
+        {
+            try
+            {
+                throw exception;
+            }
+            catch (Exception e) when (e is UserException)
+            {
+                Console.WriteLine($"{e.GetType().Name}: {e.Message}, {e.Data}, {e.HelpLink}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Исключение {e.GetType().Name}: {e.Message}");
+            }
+            finally
+            {
+                Console.WriteLine("Генерация исключений завершена");
+            }
+        }
+
+        #endregion
+
+        #region Задание 2
+
+        Console.WriteLine();
+        Console.WriteLine(new string('-', 50));
+        Console.WriteLine(" Задание №2");
+        Console.WriteLine(new string('-', 50));
+
+        var families = new string[]
+        {
+            "Иванов",
+            "Петров",
+            "Якин",
+            "Сидоров",
+            "Лебедев"
+        };
+
         var numberReader = new NumberReader();
-        numberReader.NumberEnteredEvent += ShowNumber;
+        numberReader.NumberEnteredEvent += SortArray;
         while (true)
         {
             try
             {
                 numberReader.Read();
             }
-            catch (FormatException)
+            catch (Exception ex) when (ex is UserException)
             {
                 Console.WriteLine("Введено некорректное значение!");
             }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Непредвиденная ошибка! Повторите попытку");
+            }
         }
 
-        static void ShowNumber(int number)
+        void SortArray(int number)
         {
+            SortOrder sortOrder;
             switch (number)
             {
                 case 1:
-                    Console.WriteLine("Введено значение 1");
+                    Console.WriteLine("Выбрана сортировка от А до Я");
+                    sortOrder = SortOrder.Asc;
                     break;
                 case 2:
-                    Console.WriteLine("Введено значение 2");
+                    Console.WriteLine("Выбрана сортировка от Я до А");
+                    sortOrder = SortOrder.Desc;
+                    break;
+                default:
+                    sortOrder = SortOrder.Asc;
                     break;
             }
+
+            if (sortOrder == SortOrder.Asc)
+            {
+                families = families.OrderBy(x => x).ToArray();
+            }
+            else
+            {
+                families = families.OrderByDescending(x => x).ToArray();
+            }
+
+            foreach (var family in families)
+            {
+                Console.WriteLine(family);
+            }
         }
+
+        #endregion
+    }
+
+    private enum SortOrder
+    {
+        Asc,
+        Desc
     }
 
     class NumberReader
@@ -41,13 +136,13 @@ class Program
         public void Read()
         {
             Console.WriteLine();
-            Console.WriteLine("Введите значение 1 или 2");
+            Console.WriteLine("Введите направление сортировки 1(А->Я) или 2(Я->А)");
 
             int number = Convert.ToInt32(Console.ReadLine());
 
             if (number != 1 && number != 2)
             {
-                throw new FormatException();
+                throw new UserException("Ошибка формата данных!");
             }
 
             NumberEntered(number);
